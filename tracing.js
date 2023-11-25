bd0b4b74ac091dc8fe7afe66221638409f729ccd
estelledenis <73923523+estelledenis@users.noreply.github.com>
2023-11-25 16:38:08 -0500

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -1,24 +1,34 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
-//Instrumentations
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-//Exporter
+
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
+   // Update the Jaeger exporter configuration with your Jaeger server information
+   const jaegerExporter = new JaegerExporter({
+       serviceName: serviceName,
+       endpoint: 'http://your-jaeger-collector-endpoint:14268/api/traces', // Replace with your Jaeger collector endpoint
+   });
+
+   // Create a NodeTracerProvider with Jaeger exporter
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
        }),
    });
-   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+   // Add the Jaeger exporter to the provider as a span processor
+   provider.addSpanProcessor(jaegerExporter);
+
+   // Register the tracer provider
    provider.register();
+
+   // Register instrumentations
    registerInstrumentations({
        instrumentations: [
            new HttpInstrumentation(),
@@ -27,5 +37,7 @@ module.exports = (serviceName) => {
        ],
        tracerProvider: provider,
    });
+
+   // Return the tracer
    return trace.getTracer(serviceName);
 };
